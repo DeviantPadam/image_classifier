931f22047a4aee2a6b3f05c907f3090462e88601
Padam Gupta <padamgupta1999@gmail.com>
2020-09-21 01:58:03 +0530

    Initial

---
+++ main.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 20 23:38:32 2020
+
+@author: deviantpadam
+"""
+
+
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+from model import ImageClassifier
+import torch
+from torchvision import transforms
+from PIL import Image
+
+UPLOAD_FOLDER = "static/imgs"
+ALLOWED_EXTENSIONS = { 'png', 'jpg', 'jpeg'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = ImageClassifier()
+model.load_state_dict(torch.load('weights',map_location='cpu'))
+model.eval()
+translate = {0: 'dog',
+ 1: 'horse',
+ 2: 'elephant',
+ 3: 'butterfly',
+ 4: 'chicken',
+ 5: 'cat',
+ 6: 'cow',
+ 7: 'sheep',
+ 8: 'spider',
+ 9: 'squirrel'}
+
+data_transforms = transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
+
+def get_prediction(image_bytes):
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    tensor = data_transforms(image_bytes).view(1,3,224,224).to(device)
+    outputs = model.forward(tensor)
+    prob, y_hat = torch.topk(outputs,k=5)
+    return y_hat.tolist(), prob.tolist()
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('predictions',
+                                    filename=filename))
+    return render_template('main.html')
+    
+@app.route('/predictions')
+def predictions():
+    filename = request.args.get('filename')
+    image = Image.open(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    labels, probs = get_prediction(image)
+    # print(preds)
+    # print(labels[0])
+    labels = [translate[i] for i in labels[0]]
+    return render_template('predict.html',filename=filename, labels=labels, probs=probs[0])
+    
+if __name__=="__main__":
+    app.run()
---
+++ model.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+"""
+Created on Sun Sep 20 23:38:32 2020
+
+@author: deviantpadam
+"""
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+from PIL import Image, ImageFilter
+import numpy as np
+import tqdm.auto as tqdm
+import kornia
+
+
+class ImageClassifier(nn.Module):
+    def __init__(self):
+        super(ImageClassifier,self).__init__()
+        num_classes=10
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.feature_extractor = models.resnet50(pretrained=True)
+#         self.feature_extractor.eval()
+        self.feature_extractor.fc = nn.Linear(self.feature_extractor.fc.in_features,out_features=num_classes)
+#         self.classifier = nn.Linear(in_features=1000,out_features=num_classes)
+        
+        
+        self.data_transforms = transforms.Compose([transforms.RandomResizedCrop(224),
+                                                   transforms.RandomHorizontalFlip(),
+                                                   transforms.ToTensor(),
+                                                   transforms.Normalize([0.485, 0.456, 0.406], 
+                                                                         [0.229, 0.224, 0.225])])
+        
+        self.translate = {'cane': 'dog',
+                          'cavallo': 'horse',
+                          'elefante': 'elephant',
+                          'farfalla': 'butterfly',
+                          'gallina': 'chicken',
+                          'gatto': 'cat',
+                          'mucca': 'cow',
+                          'pecora': 'sheep', 
+                          'scoiattolo': 'squirrel',
+                          'ragno': 'spider'}
+        
+    def forward(self,x):
+        embeddings = self.feature_extractor(x)
+        x = embeddings
+#         x = self.classifier(embeddings)
+        return x
+    
+    def fit(self,root_dir,num_epochs,batch_size,val_split=0.1,num_workers=4):
+        image_datasets = datasets.ImageFolder(root_dir,self.data_transforms)
+        image_datasets.classes = [self.translate[i] for i in image_datasets.classes]
+        train_size = int((1-val_split)*len(image_datasets))
+        train, valid = torch.utils.data.random_split(image_datasets,[train_size,len(image_datasets)-train_size])
+        dataloaders = {'train': torch.utils.data.DataLoader(train, batch_size=batch_size,
+                                             shuffle=True, num_workers=num_workers),
+                       'val': torch.utils.data.DataLoader(valid, batch_size=batch_size,
+                                             shuffle=True, num_workers=num_workers)}
+        
+        criterion = nn.CrossEntropyLoss()
+        optimizer = optim.Adam(self.parameters(),lr=0.0001)
+        epoch_bar = tqdm.tqdm(range(num_epochs),desc='Epochs---',position=0)
+        self.train_epoch_loss = []
+        self.val_epoch_loss = []
+        train_acc = 0.
+        val_acc = 0.
+        for epoch in epoch_bar:
+            self.train()
+            pbar = tqdm.tqdm(dataloaders['train'],desc='Epoch= {}'.format(epoch+1),position=1)
+            running_loss = 0.0
+            val_loss = 0.0
+            total = 0
+            correct = 0
+            for inputs, labels in pbar:
+                inputs = inputs.to(self.device)
+                labels = labels.to(self.device)
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+                labels_hat = self(inputs)
+                _, predicted = torch.max(labels_hat.data, 1)
+                loss = criterion(labels_hat,labels)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                pbar.set_postfix(train_run_loss='{:.4f}'.format(loss.item()))
+                running_loss += loss.item()
+                loss.backward()
+                optimizer.step()
+            self.train_epoch_loss.append(running_loss / len(dataloaders['train']))
+            train_acc = 100 * correct / total
+            epoch_bar.set_postfix(train_acc='{:.4f}'.format(train_acc),val_acc='{:.4f}'.format(val_acc))    
+#             pbar = tqdm.tqdm(dataloaders['val'],desc='Epoch={}, train_epoch_loss= {}'.format(epoch+1,epoch_loss))
+            total = 0
+            correct = 0
+            with torch.no_grad():
+                self.eval()
+                for inputs, labels in dataloaders['val']:
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
+                    labels_hat = self(inputs)
+                    _, predicted = torch.max(labels_hat.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
+                    loss = criterion(labels_hat,labels)
+                    val_loss += loss.item()
+                self.val_epoch_loss.append(val_loss / len(dataloaders['val']))
+                val_acc = 100 * correct / total
+                epoch_bar.set_postfix(train_acc='{:.4f}'.format(train_acc),val_acc='{:.4f}'.format(val_acc))   
+
+
